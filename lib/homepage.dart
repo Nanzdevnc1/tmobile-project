b941d56cb909949d6e5660a124ff1ad73ffad66b
Nanzdevnc1 <220605110112@student.uin-malang.ac.id>
2024-10-02 11:37:53 +0700

    Working on Main Page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,35 +12,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // Menyimpan indeks dari kontainer yang sedang dipilih
   int? selectedIndex;
+  bool isLoved = false;
+  bool isLoved1 = false;
+  bool isLoved2 = false;
+
+  int _selectedIndex = 0;
+
+    void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5F5F5), // Warna dasar Scaffold yang tidak terlalu putih
-      appBar: AppBar(
-        backgroundColor: Color(0xFFF5F5F5),
-        title: Row(
-          children: [
-            SvgPicture.asset(
-              'assets/location.svg',
-              width: 20,
-              height: 20,
-            ),
-            SizedBox(width: 5),
-            Text(
-              'Bali, Indonesia',
-              style: GoogleFonts.gowunBatang(
-                  fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Expanded(child: SizedBox(width: 5)),
-            SvgPicture.asset('assets/notification.svg', width: 18, height: 25),
-            SizedBox(width: 8),
-            SvgPicture.asset('assets/send.svg', width: 30, height: 35),
-          ],
+        backgroundColor:
+            Color(0xFFF5F5F5), // Warna dasar Scaffold yang tidak terlalu putih
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF5F5F5),
+          title: Row(
+            children: [
+              SvgPicture.asset(
+                'assets/location.svg',
+                width: 20,
+                height: 20,
+              ),
+              SizedBox(width: 5),
+              Text(
+                'Bali, Indonesia',
+                style: GoogleFonts.gowunBatang(
+                    fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Expanded(child: SizedBox(width: 5)),
+              SvgPicture.asset('assets/notification.svg',
+                  width: 18, height: 25),
+              SizedBox(width: 8),
+              SvgPicture.asset('assets/send.svg', width: 30, height: 35),
+            ],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+        body: SingleChildScrollView(
+            child: Column(
           children: [
             // Stack untuk gambar dengan teks dan gradasi gelap di samping kiri
             Stack(
@@ -67,7 +80,8 @@ class _HomePageState extends State<HomePage> {
                       begin: Alignment.centerLeft,
                       end: Alignment.centerRight,
                       colors: [
-                        Colors.black.withOpacity(0.9), // Warna hitam transparan di kiri
+                        Colors.black
+                            .withOpacity(0.9), // Warna hitam transparan di kiri
                         Colors.transparent, // Transparan di kanan
                       ],
                     ),
@@ -146,7 +160,9 @@ class _HomePageState extends State<HomePage> {
                       width: 80,
                       height: 80,
                       decoration: BoxDecoration(
-                        color: selectedIndex == 0 ? Colors.pinkAccent : Colors.white,
+                        color: selectedIndex == 0
+                            ? Colors.pinkAccent
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: Column(
@@ -176,14 +192,16 @@ class _HomePageState extends State<HomePage> {
                       width: 80,
                       height: 80,
                       decoration: BoxDecoration(
-                        color: selectedIndex == 1 ? Colors.pinkAccent : Colors.white,
+                        color: selectedIndex == 1
+                            ? Colors.pinkAccent
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           SvgPicture.asset(
-                            'assets/rental.svg',
+                            'assets/rental2.svg',
                             width: 50,
                             height: 40,
                           ),
@@ -206,7 +224,9 @@ class _HomePageState extends State<HomePage> {
                       width: 80,
                       height: 80,
                       decoration: BoxDecoration(
-                        color: selectedIndex == 2 ? Colors.pinkAccent : Colors.white,
+                        color: selectedIndex == 2
+                            ? Colors.pinkAccent
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: Column(
@@ -236,7 +256,9 @@ class _HomePageState extends State<HomePage> {
                       width: 80,
                       height: 80,
                       decoration: BoxDecoration(
-                        color: selectedIndex == 3 ? Colors.pinkAccent : Colors.white,
+                        color: selectedIndex == 3
+                            ? Colors.pinkAccent
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: Column(
@@ -266,7 +288,9 @@ class _HomePageState extends State<HomePage> {
                       width: 80,
                       height: 80,
                       decoration: BoxDecoration(
-                        color: selectedIndex == 4 ? Colors.pinkAccent : Colors.white,
+                        color: selectedIndex == 4
+                            ? Colors.pinkAccent
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(15),
                       ),
                       child: Column(
@@ -286,9 +310,353 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Below are Several Beautiful',
+                        style: GoogleFonts.baumans(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Destinations in Bali',
+                        style: GoogleFonts.baumans(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(child: SizedBox(width: 10)),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pinkAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical:
+                              8), // Padding lebih kecil untuk tombol 'Show More'
+                    ),
+                    child: Text(
+                      'Show More',
+                      style: GoogleFonts.carterOne(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12, // Ukuran font lebih kecil dari 'Book Now'
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            Stack(
+              children: [
+                // Gambar sebagai latar belakang
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: AssetImage('assets/GWK.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // Gradasi gelap di atas gambar
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.black.withOpacity(
+                            0.5), // Gradasi hitam transparan di atas
+                        Colors.transparent, // Transparan di bawah
+                      ],
+                    ),
+                  ),
+                ),
+
+                // Teks dan ikon lokasi di kiri atas (gunakan ikon SVG)
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: Row(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.center, // Sejajarkan vertikal
+                    children: [
+                      SvgPicture.asset(
+                        'assets/place.svg', // Ganti dengan path ikon lokasi SVG Anda
+                        width: 20,
+                        height: 20,
+                        color: Colors.white, // Pastikan warna sesuai
+                      ),
+                      SizedBox(width: 5), // Jarak antara ikon lokasi dan teks
+                      Text(
+                        'GWK',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Ikon love di kanan atas dengan GestureDetector untuk menangani tap
+                Positioned(
+                  top:
+                      25, // Pastikan nilai top sama dengan ikon dan teks di sebelah kiri
+                  right: 20,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isLoved = !isLoved; // Toggle state ketika ikon di-tap
+                      });
+                    },
+                    child: SvgPicture.asset(
+                      'assets/love.svg', // Ganti dengan path ikon love SVG Anda
+                      width: 16, // Ukuran width untuk ikon love
+                      height: 16, // Ukuran height untuk ikon love
+                      color: isLoved
+                          ? Colors.pink
+                          : Colors.white, // Ubah warna sesuai state
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+  children: [
+    Expanded(
+      child: Stack(
+        children: [
+          // Gambar 1 (Pura Uluwatu)
+          Container(
+            margin: EdgeInsets.all(10.0),
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image: AssetImage('assets/pu.jpg'), // Ganti dengan gambar Pura Uluwatu
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          // Gradasi dan ikon love pada gambar 1
+          Container(
+            margin: EdgeInsets.all(10.0),
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black.withOpacity(0.7),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  'assets/place.svg',
+                  width: 15,
+                  height: 15,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 5), // Jarak antara ikon lokasi dan teks
+                Text(
+                  'Pura Uluwatu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12, // Teks diperkecil
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isLoved1 = !isLoved1;
+                });
+              },
+              child: SvgPicture.asset(
+                'assets/love.svg',
+                width: 14,
+                height: 14,
+                color: isLoved1 ? Colors.pink : Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+    Expanded(
+      child: Stack(
+        children: [
+          // Gambar 2 (Pura Besakih)
+          Container(
+            margin: EdgeInsets.all(10.0),
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image: AssetImage('assets/pb.webp'), // Ganti dengan gambar Pura Besakih
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          // Gradasi dan ikon love pada gambar 2
+          Container(
+            margin: EdgeInsets.all(10.0),
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black.withOpacity(0.7),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  'assets/place.svg',
+                  width: 15,
+                  height: 15,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 5), // Jarak antara ikon lokasi dan teks
+                Text(
+                  'Pura Besakih',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12, // Teks diperkecil
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isLoved2 = !isLoved2;
+                });
+              },
+              child: SvgPicture.asset(
+                'assets/love.svg',
+                width: 14,
+                height: 14,
+                color: isLoved2 ? Colors.pink : Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  ],
+),
+
+          ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.share),
+            label: 'Share',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_offer),
+            label: 'Promotion',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildCategoryCard(String iconPath, String title, int index) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        margin: const EdgeInsets.all(10.0),
+        width: 80,
+        height: 80,
+        decoration: BoxDecoration(
+          color: selectedIndex == index ? Colors.pinkAccent : Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              iconPath,
+              width: 50,
+              height: 40,
+            ),
+            const SizedBox(height: 10),
+            Text(title),
           ],
         ),
       ),
     );
   }
 }
+
+
